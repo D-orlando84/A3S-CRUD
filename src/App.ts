de2c797969b727606b0998cd5a9b3cd0032a471b
D-orlando84 <orlando.ducamp1@gmail.com>
2020-12-01 21:48:13 +0100

    commit

--- src/App.ts
+++ src/App.ts
@@ -21,8 +21,6 @@ class App {
             hostname: "https://" + config.url,
             cacheTime: 600000
         });
-
-        const data = await this.pagesDAO.getPages();
               
         this.express.use(json());
         this.express.use(raw());
@@ -45,10 +43,33 @@ class App {
         this.express.set('view engine', 'pug');
         this.express.set('view cache', true);
 
-        router.get('/sitemap.xml', function (req, res) {
-            res.setHeader('Content-Type', 'application/xml');
-            res.send(sm.toString());
+        router.get('/', async function (req, res) {
+            res.setHeader('Content-Type', 'text/html');
+            const data = await new PagesDAO().getPages();
+            let page = [];
+            for (let i=0;i<data.length;i++){
+                page.push(data[i]);
+            }
+            return res.render('index',{'data': page});
         });
+        router.get('/modif', async function (req,res){
+            const id= req.query._id;
+            //j'appelle la base de données, dans la collection pages pour récupérer tout le contenu dont l'id correspond à id
+            const data = await new PagesDAO().getPageId(id);
+            return res.render('modif',{'page': data[0]});
+        });
+        router.post('/modif', async function (req,res){
+            const data = await new PagesDAO().postPage();
+            const item = {
+                id: req.body.id,
+                title: req.body.title,
+                content: req.body.content,
+                order: req.body.order,
+                type: req.body.type,
+                meta_title: req.body.meta_title,
+                meta_description: req.body.meta_description
+            }
+        })
     }
 };
 
--- src/dao/PagesDAO.ts
+++ src/dao/PagesDAO.ts
@@ -11,11 +11,34 @@ class PagesDAO extends DAO {
         try {
             const db = await super.getDb();
             const collection = db.collection('pages');
-            const pages = await collection.find().sort({ order: 1 }).toArray();
-            console.log(pages);
+            const pages = await collection.find({"type": 'page'}).sort({ order: 1 }).toArray();
             return pages;
         } catch (err) {
             throw new Error("Impossible de récupérer les pages." + err);
+        }
+    }
+
+    public async getPageId(_id){
+        try {
+            const db = await super.getDb();
+            const collection = db.collection('pages');
+            const pagesId = await collection.find({"_id":_id}).toArray();
+            return pagesId;
+
+        } catch (err) {
+            throw new Error("Impossible de trouver la page" + err);
+        }
+    }
+
+    public async postPage(){
+        try{
+            const db = await super.getDb();
+            const collection = db.collection('pages'); 
+            const doc = {id: "toto", title: "", content: "", order: 1, type: "page", meta_title: "Mention Légles", meta_description: ""}
+            const result = await collection.insertOne(doc);
+            return result;
+        } catch(err){
+            throw new Error("Impossible d'insérer du contenue");
         }
     }
 }
